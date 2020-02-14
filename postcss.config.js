b7f51cf30d06ad387347b42a35dfe296d745ec7e
Roman Vereshchagin <info@sibvrv.com>
2020-02-15 02:09:29 +0700

    autoprefixer plugin update

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 ﻿module.exports = {
   parser: false,
   plugins: {
-    'autoprefixer': {
-      browsers: ['last 2 versions']
-    },
     'postcss-import': {},
     'postcss-preset-env': {},
     'cssnano': {}
